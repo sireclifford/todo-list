14ca558edb9cb9f1884981d57a070dfcd086f441
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-14 15:51:01 +0430

    test case 1 exported

--- util.test.js
+++ util.test.js
@@ -23,3 +23,12 @@ import {
       ];
       const quest = (id) => listArray.filter((item) => item.id !== id);
   
+      expect(addTodo('test')).toBe(1);
+      expect(addTodo('test2')).toBe(1);
+      expect(addTodo('test3')).toBe(1);
+      expect(deleteTodo(0)).toEqual(quest(0));
+      expect(deleteTodo(1)).toEqual(quest(1));
+      expect(deleteTodo(2)).toEqual(quest(2));
+    });
+  });
+  
