3bf7d0019bd60a07de0ba6afb7638f13e5c2086f
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-14 15:49:57 +0430

    test case 1 added

--- util.test.js
+++ util.test.js
@@ -1,3 +1,25 @@
 import {
     addTodo, deleteTodo, updateTodo, removeTodo,
   } from './util.js';
+  
+  describe('add_delete_test', () => {
+    it('add_delete_Todo', () => {
+      const listArray = [
+        {
+          id: 0,
+          title: 'test',
+          completed: false,
+        },
+        {
+          id: 1,
+          title: 'test2',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'test3',
+          completed: false,
+        },
+      ];
+      const quest = (id) => listArray.filter((item) => item.id !== id);
+  
